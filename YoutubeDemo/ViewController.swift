f72d04fada6cca8f5dce6721c7339b0d971a4ebe
Scott Horsfall <scohorsfall@gmail.com>
2016-06-07 11:56:53 -0700

    Initial Commit

---
+++ YoutubeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YoutubeDemo
+//
+//  Created by Scott Horsfall on 6/7/16.
+//  Copyright © 2016 Scott Horsfall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
